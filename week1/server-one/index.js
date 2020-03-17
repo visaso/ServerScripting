83f78dd74c4f1ba83a02ec09b19e50ebc94ae828
visaso <32449867+visaso@users.noreply.github.com>
2020-03-17 10:16:38 +0200

    Static content

--- week1/server-one/index.js
+++ week1/server-one/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/static', express.static('public'))
+
+app.get('/', (req, res) => res.send(' World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
