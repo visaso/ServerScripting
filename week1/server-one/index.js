fb2c004991b1aaf0b6387165085b0940b88a6f3d
visaso <32449867+visaso@users.noreply.github.com>
2020-03-17 09:50:04 +0200

    Init

---
+++ week1/server-one/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('/catinfo', (req, res) => {
+    const cat = {
+      'name': 'Frank',
+      'age': 6,
+      'weight': 5,
+    };
+    res.json(cat);
+  });
